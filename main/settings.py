a921faebc2e6fb9d0db0c2dc0f4f17825cf175ec
realUmasorDavid <145034172+realUmasorDavid@users.noreply.github.com>
2026-06-25 11:44:29 +0100

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -145,11 +145,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-AWS_ACCESS_KEY_ID = os.getenv("SUPABASE_ACCESS_KEY_ID", "placeholder-key")
-AWS_SECRET_ACCESS_KEY = os.getenv("SUPABASE_SECRET_ACCESS_KEY", "placeholder-secret")
-AWS_STORAGE_BUCKET_NAME = os.getenv("SUPABASE_STORAGE_BUCKET_NAME", "placeholder-bucket")  
-AWS_S3_ENDPOINT_URL = os.getenv("SUPABASE_S3_ENDPOINT_URL", "https://supabase.co")          
-AWS_S3_REGION = os.getenv("SUPABASE_S3_REGION", "eu-central-1")           
+AWS_ACCESS_KEY_ID = os.getenv("SUPABASE_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("SUPABASE_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("SUPABASE_STORAGE_BUCKET_NAME")  
+AWS_S3_ENDPOINT_URL = os.getenv("SUPABASE_S3_ENDPOINT_URL")          
+AWS_S3_REGION = os.getenv("SUPABASE_S3_REGION")          
 
 AWS_S3_SIGNATURE_VERSION = 's3v4'   
 AWS_S3_ADDRESSING_STYLE = 'path'    
@@ -160,42 +160,42 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=315360000',
 }
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "storages.backends.s3.S3Storage",
-        "OPTIONS": {
-            "endpoint_url": AWS_S3_ENDPOINT_URL,
-            "bucket_name": AWS_STORAGE_BUCKET_NAME,
-            "location": "static",
-            "addressing_style": "path",
-            "default_acl": None,
-            "querystring_auth": True,
-            "file_overwrite": True,
-        },
-    },
-    "default": {
-        "BACKEND": "storages.backends.s3.S3Storage",
-        "OPTIONS": {
-            "access_key": AWS_ACCESS_KEY_ID,
-            "secret_key": AWS_SECRET_ACCESS_KEY,
-            "bucket_name": AWS_STORAGE_BUCKET_NAME,
-            "endpoint_url": AWS_S3_ENDPOINT_URL,
-            "location": "media",
-            "region_name": AWS_S3_REGION,
-            "signature_version": AWS_S3_SIGNATURE_VERSION,
-        },
-    },
-}
+# STORAGES = {
+#     "staticfiles": {
+#         "BACKEND": "storages.backends.s3.S3Storage",
+#         "OPTIONS": {
+#             "endpoint_url": AWS_S3_ENDPOINT_URL,
+#             "bucket_name": AWS_STORAGE_BUCKET_NAME,
+#             "location": "static",
+#             "addressing_style": "path",
+#             "default_acl": None,
+#             "querystring_auth": True,
+#             "file_overwrite": True,
+#         },
+#     },
+#     "default": {
+#         "BACKEND": "storages.backends.s3.S3Storage",
+#         "OPTIONS": {
+#             "access_key": AWS_ACCESS_KEY_ID,
+#             "secret_key": AWS_SECRET_ACCESS_KEY,
+#             "bucket_name": AWS_STORAGE_BUCKET_NAME,
+#             "endpoint_url": AWS_S3_ENDPOINT_URL,
+#             "location": "media",
+#             "region_name": AWS_S3_REGION,
+#             "signature_version": AWS_S3_SIGNATURE_VERSION,
+#         },
+#     },
+# }
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_URL = f"{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/static/"
-# STATIC_URL = '/static/'
+# STATIC_URL = f"{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/static/"
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = f"{AWS_S3_ENDPOINT_URL}/storage/v1/object/public/{AWS_STORAGE_BUCKET_NAME}/"
-# MEDIA_URL = '/media/' 
+# MEDIA_URL = f"{AWS_S3_ENDPOINT_URL}/storage/v1/object/public/{AWS_STORAGE_BUCKET_NAME}/"
+MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
